58714d58104185d02ceb902bc65b46a5973def53
goeunkim <akfmdl@naver.com>
2021-09-29 23:58:50 +0900

    Create consts.py

---
+++ consts.py
@@ -0,0 +1,5 @@
+PARAMETER_PATH = "./inspection/parameters.json"
+MODEL_INFO_PATH = "./inspection/model_info.json"
+MODEL_PATH = "./models"
+RESULT_INFO_PATH = "./result.csv"
+SOUND_EXTENSION = [".tdms"]
